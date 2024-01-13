2b6191ce52a2a915344185c6f0a80757d49fe7ff
jpekblue16 <151572445+jpekblue16@users.noreply.github.com>
2024-01-13 23:10:16 +0000

    complete day5 part 2

--- day5/day5_part2.js
+++ day5/day5_part2.js
@@ -1,4 +1,5 @@
 const fs = require('node:fs');
+const { sourceMapsEnabled } = require('node:process');
 const readline = require('readline');
 
 // step 0: read seeds
@@ -13,13 +14,15 @@ var steps = 0;
 
 var seeds = [];
 
-var seedToSoilMap = [];
-var soilToFertMap = [];
-var fertToWaterMap = [];
-var waterToLightMap = [];
-var lightToTempMap = [];
-var tempToHumidityMap = [];
-var humidityToLocMap = [];
+var maps = [
+    [], // seed to soil
+    [], // soil to fertilizer
+    [], // fertilizer to water
+    [], // water to light
+    [], // light to temp
+    [], // temp to humidity
+    []  // humidity to location
+]
 
 const file = readline.createInterface({
     input: fs.createReadStream('day5_input.txt'),
@@ -39,62 +42,18 @@ file.on('line', (line) => {
             console.log(seeds);
             break;
         case 1:
-            // seed to soil map
-            var values = line.split(' ').map(Number); // destination start point, source start point, number of elements
-            console.log(`destination start: ${values[0]}, source start: ${values[1]}, length: ${values[2]}`);
-            seedToSoilMap.push({
-                sourceStart: values[1],
-                destStart: values[0],
-                len: values[2]
-            });
-            break;
         case 2:
-            var values = line.split(' ').map(Number); // destination start point, source start point, number of elements
-            console.log(`destination start: ${values[0]}, source start: ${values[1]}, length: ${values[2]}`);
-            soilToFertMap.push({
-                sourceStart: values[1],
-                destStart: values[0],
-                len: values[2]
-            });
-            break;
         case 3:
-            var values = line.split(' ').map(Number); // destination start point, source start point, number of elements
-            fertToWaterMap.push({
-                sourceStart: values[1],
-                destStart: values[0],
-                len: values[2]
-            });
-            break;
         case 4:
-            var values = line.split(' ').map(Number); // destination start point, source start point, number of elements
-            waterToLightMap.push({
-                sourceStart: values[1],
-                destStart: values[0],
-                len: values[2]
-            });
-            break;
         case 5:
-            var values = line.split(' ').map(Number); // destination start point, source start point, number of elements
-            lightToTempMap.push({
-                sourceStart: values[1],
-                destStart: values[0],
-                len: values[2]
-            });
-            break;
         case 6:
-            var values = line.split(' ').map(Number); // destination start point, source start point, number of elements
-            tempToHumidityMap.push({
-                sourceStart: values[1],
-                destStart: values[0],
-                len: values[2]
-            });
-            break;
         case 7:
             var values = line.split(' ').map(Number); // destination start point, source start point, number of elements
-            humidityToLocMap.push({
+            maps[steps-1].push({
                 sourceStart: values[1],
+                sourceEnd: values[1] + values[2] - 1,
                 destStart: values[0],
-                len: values[2]
+                destEnd: values[0] + values[2] - 1
             });
             break;
         default:
@@ -113,63 +72,113 @@ file.on('line', (line) => {
 file.on('close', () => { 
     var result = -1;
 
-    // process first seed range
-    for (var seed = seeds[0]; seed < (seeds[0]+seeds[1]); ++seed) {
-        // go through mapping chain to find location
-        var soil = getValue(seed,seedToSoilMap);
-        var fertilizer = getValue(soil,soilToFertMap);
-        var water = getValue(fertilizer,fertToWaterMap);
-        var light = getValue(water,waterToLightMap);
-        var temp = getValue(light,lightToTempMap);
-        var humidity = getValue(temp,tempToHumidityMap);
-        var location = getValue(humidity,humidityToLocMap);
-        if (result < 0 || location < result) result = location;
+    // sort maps
+    for (var map in maps) {
+        maps[map].sort((a,b) => a.sourceStart - b.sourceStart);
+        if (maps[map][0].sourceStart != 0) {
+            maps[map].unshift({sourceStart:0, sourceEnd:maps[map][0].sourceStart-1 , destStart:0, destEnd: maps[map][0].sourceStart-1});
+        }
+    }
+    console.log(maps);
+
+    // split seeds into ranges and offset due to mapping
+    var ranges = [];
+    for (var seed=0,len=1;seed<seeds.length-1;seed+=2,len+=2) {
+        ranges.push({start:seeds[seed],end:seeds[seed]+seeds[len]-1});
+    }
+    ranges.sort((a,b) => a.start - b.start);
+    console.log(ranges);
+
+    // for each map, split ranges
+    for (var map of maps) {
+        ranges = mapSourceToDest(map,ranges);
+        console.log(ranges);
     }
 
-    for (var seed = seeds[2]; seed < (seeds[2]+seeds[3]); ++seed) {
-        //console.log(`processing seed: ${seed}`);
-        // go through mapping chain to find location
-        var soil = getValue(seed,seedToSoilMap);
-        var fertilizer = getValue(soil,soilToFertMap);
-        var water = getValue(fertilizer,fertToWaterMap);
-        var light = getValue(water,waterToLightMap);
-        var temp = getValue(light,lightToTempMap);
-        var humidity = getValue(temp,tempToHumidityMap);
-        var location = getValue(humidity,humidityToLocMap);
-        if (result < 0 || location < result) result = location;
-    }
+    // check low end of each range
+    ranges.sort((a,b) => a.start - b.start);
+    console.log(ranges);
 
-    console.log('lowest location value: '+result);
+    console.log('lowest location value: '+ranges[0].start);
 });
 
-function getValue(source,map) {
-    for (var mapping of map) {
-        if (mapping.sourceStart <= source && source < mapping.sourceStart + mapping.len) {
-            return mapping.destStart + (source - mapping.sourceStart);
+// returns the new range divided based on the given map
+// ranges is: array of objects { start: x, end: y }
+function mapSourceToDest(map,ranges) {
+
+    var resultArray = [];
+
+    for (var sourceRange of Object.values(ranges)) {
+
+        // split the current range based on the map
+        var splitRange = processSingleRange(sourceRange,map);
+
+        resultArray.push.apply(resultArray,splitRange);
+
+    }
+
+    //output new array of seed ranges
+    return resultArray;
+}
+
+// returns an array of range objects, split based on the map
+/*
+  { 6 7   8 9 }
+1 2 3 4 | 5 6 7 8
+
+becomes
+
+[{ 3 4 }, { 5 6 }]
+*/
+// range has: start, end
+// mapRange has: sourceStart, sourceEnd, destStart, destEnd
+function processSingleRange(range,map) {
+    console.log('current range is: ',range);
+
+    var curStart = range.start;
+    var hangingRange = false; // tracks if there is a range that was split that still needs to be added
+
+    var splitRange = []; // array to return if range is split into 2 or more ranges
+
+    for (var mapRange of Object.values(map)) {
+        console.log('checking map range: ',mapRange);
+
+        // compare source range to map range
+        var offset = (mapRange.destStart - mapRange.sourceStart);
+
+        // case 1: full overlap (range is entirely within map range) - return dest range values over source range
+        if (curStart >= mapRange.sourceStart && range.end <= mapRange.sourceEnd) {
+
+            // convert source values to dest values
+            var newStart = curStart + offset;
+            var newEnd = range.end + offset;
+
+            splitRange.push({start:newStart,end:newEnd});
+            hangingRange = false;
+            break;
+        }
+
+        // case 3: partial overlap - end goes past map range
+        else if ( (curStart >= mapRange.sourceStart && curStart <= mapRange.sourceEnd) && range.end > mapRange.sourceEnd ) {
+            // split range into { start, sourceEnd } and { sourceEnd+1, end }
+            // convert first range to dest values
+            var newRangeStart = curStart + offset;
+            var newRangeEnd = mapRange.sourceEnd + offset;
+            splitRange.push({start:newRangeStart,end:newRangeEnd});
+
+            // set curStart to sourceEnd+1 and move to next map range - the next range will contain curStart
+            curStart = mapRange.sourceEnd + 1;
+            hangingRange = true;
         }
     }
 
-    return source; // if the source value wasnt found in the defined mapping, return same value
+    // if no ranges are added to the output array, return range
+    if (splitRange.length == 0) {
+        splitRange.push(range);
+    } else if (hangingRange) { // if a range was split and there is still a range to add, add it to the end
+        splitRange.push({start:curStart,end:range.end});
+    }
+
+    return splitRange;
+
 }
-
-
-
-// function main() {
-//     // process seeds and find lowest location number
-//     for (var seed of seeds) {
-//         // go through mapping chain to find location
-//         var soil = getValue(seed,seedToSoilMap);
-//         var fertilizer = getValue(soil,soilToFertMap);
-//         var water = getValue(fertilizer,fertToWaterMap);
-//         var light = getValue(water,waterToLightMap);
-//         var temp = getValue(light,lightToTempMap);
-//         var humidity = getValue(temp,tempToHumidityMap);
-//         var location = getValue(humidity,humidityToLocMap);
-//         if (result < 0 || location < result) result = location;
-//     }
-// }
-
-// main();
-
-
-
