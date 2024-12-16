64cfe6d4553f714d1049f8d8a54eb09dbb9c9474
Jenna <pek.jenna16@gmail.com>
2024-12-15 17:51:05 -0800

    finish day3

---
+++ 2024/day3/day3_part1.js
@@ -0,0 +1,36 @@
+// find all mul(X,Y) instances
+// perform multiplication and sum
+
+//const INPUT = `xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))`;
+
+var INPUT = "";
+
+const fs = require('node:fs');
+const readline = require('readline');
+
+const file = readline.createInterface({
+    input: fs.createReadStream('day3_input.txt'),
+    output: process.stdout,
+    terminal: false
+});
+
+file.on('line',(line) => INPUT += line);
+
+file.on('close', () => {
+    var mulRegex = /(mul\([0-9]+,[0-9]+\))/g;
+    var numRegex = /[0-9]+/g;
+    
+    var muls = Array.from(INPUT.matchAll(mulRegex),(v) => v[0]);
+    
+    var total = 0;
+    
+    for (var mul of muls) {
+        var operands = Array.from(mul.matchAll(numRegex),(v) => parseInt(v[0]));
+    
+        total += operands[0] * operands[1];
+    }
+    
+    console.log(total);
+})
+
+
