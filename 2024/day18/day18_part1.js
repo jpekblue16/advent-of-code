3cd266bb78025c9dbdae0fa23ef9a3af9cba4e52
Jenna <pek.jenna16@gmail.com>
2025-01-19 22:20:19 -0800

    complete day 18 part 1 (BFS)

--- 2024/day18/day18_part1.js
+++ 2024/day18/day18_part1.js
@@ -2,12 +2,12 @@ const fs = require('node:fs');
 const readline = require('readline');
 
 const file = readline.createInterface({
-  input: fs.createReadStream('day18_test.txt'),
+  input: fs.createReadStream('day18_input.txt'),
   output: process.stdout,
   terminal: false,
 });
 
-var GRID_SIZE = 7;
+var GRID_SIZE = 71;
 
 var BLOCK_COUNT = 1024;
 
@@ -29,59 +29,137 @@ class vec {
   neg() {
     return new vec(-this.x, -this.y);
   }
-}
 
-class cell {
-  // Row and Column index of its parent
-  // Note that 0 <= i <= ROW-1 & 0 <= j <= COL-1
-  constructor() {
-    this.parent_i = -1;
-    this.parent_j = -1;
-    this.f = Infinity;
-    this.g = Infinity;
-    this.h = Infinity;
+  isEqual(otherVec) {
+    return this.x == otherVec.x && this.y == otherVec.y;
   }
 }
 
 // read in locations of first BLOCK_COUNT blocks
 var blocksRead = 0;
-var blockGrid = new Array(GRID_SIZE).fill(new Array(GRID_SIZE).fill(1));
+var blocks = new Set();
 file.on('line', (line) => {
   // only read first designated amount of blocks
   if (blocksRead >= BLOCK_COUNT) return;
 
-  var point = Array.from(line.split(','), (v) => parseInt(v));
-  // set grid block
-  blockGrid[point[1]][point[0]] = 0;
+  blocks.add(line);
+  blocksRead++;
 });
 
 file.on('close', () => {
-  // create the grid, setting blocks at each point
-  var grid = initGrid();
-
-  // a* search proritize S and E moves
+  // a* search, proritize S and E moves
   var start = new vec(0, 0);
   var end = new vec(GRID_SIZE - 1, GRID_SIZE - 1);
 
-  var cost = doAStar(start, end);
+  var cost = findShortestPath(start, end);
 
   console.log(cost);
 });
 
-function doAStar(start, end) {
-  // create closed list
-  var closedList = new Array(GRID_SIZE)
-    .fill()
-    .map(() => new Array(GRID_SIZE).fill(false));
+// finds the shortest path from start to end, navigating around blocks
 
-  var detailGrid = new Array(GRID_SIZE)
-    .fill()
-    .map(() => new Array().fill(new cell()));
+var grid;
+function findShortestPath(start, end) {
+  // create grid
+  // each point has
+  // - id for tracking cost to reach
+  // - steps taken to reach point
+  // - blocked or open
+  initGrid();
+
+  // do breadth first search
+  var searchQueue = [start];
+  getGridCell(start).steps = 0;
+  var curStep = 0;
+  while (searchQueue.length > 0) {
+    // take the next point in the queue
+    var cur = searchQueue.shift();
+    var curCell = getGridCell(cur);
+
+    // if steps > curstep, on next step (should only ever increment by 1)
+    if (curCell.steps > curStep) curStep = curCell.steps;
+
+    // check neighboring open cells
+    var adjacent = getAllNext(cur);
+    for (var next of adjacent) {
+      var nextCell = getGridCell(next);
+      //   if destination return curstep+1
+      if (next.isEqual(end)) return curStep + 1;
+      //   else if curstep+1 < next.steps add to search queue with steps = curStep+1
+      else if (curStep + 1 < nextCell.steps) {
+        nextCell.steps = curStep + 1;
+        searchQueue.push(next);
+      }
+    }
+  }
+
+  // end not found (end is guaranteed to be found)
+  return -1;
 }
 
-// return the grid value of the point
-function getGridNode(p) {
+class gridCell {
+  constructor(id_in) {
+    this.id = id_in;
+    this.steps = Infinity;
+    this.blocked = false;
+  }
+}
+
+// initialize the traversal grid
+function initGrid() {
+  var newGrid = new Array(GRID_SIZE).fill().map(() => new Array(GRID_SIZE));
+
+  var idCounter = 0;
+  for (var row in newGrid) {
+    for (var col = 0; col < GRID_SIZE; ++col) {
+      newGrid[row][col] = new gridCell(idCounter++);
+      if (blocks.has(`${col},${row}`)) newGrid[row][col].blocked = true;
+    }
+  }
+
+  grid = newGrid;
+}
+
+// return the grid cell at the given point
+function getGridCell(p) {
   return grid[p.y][p.x];
+}
+
+// find all points adjacent to the given point that are traversable
+const DIRECTIONS = ['e', 's', 'w', 'n'];
+function getAllNext(p) {
+  // check each direction, if valid, add to result
+  var result = [];
+  for (var dir of DIRECTIONS) {
+    var next = getNext(p, dir);
+
+    if (isInsideGrid(next) && !grid[next.y][next.x].blocked) {
+      result.push(next);
+    }
+  }
+
+  return result;
+}
+
+// gets the coordinates for the next space in the given direction
+function getNext(pos, dir) {
+  return pos.add(getDir(dir));
+}
+
+// returns the unit vector for the given direction
+function getDir(dir) {
+  switch (dir) {
+    case 'n':
+      return new vec(0, -1);
+    case 'e':
+      return new vec(1, 0);
+    case 's':
+      return new vec(0, 1);
+    case 'w':
+      return new vec(-1, 0);
+    default:
+      throw 'BAD';
+  }
 }
 
 // determine if the point is inside the grid
