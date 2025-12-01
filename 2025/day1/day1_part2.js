bb228cfb08580d7dde2c9f221f043944facdee5e
jpekblue16 <pek.jenna16@gmail.com>
2025-12-01 13:45:07 -0800

    day1 finished

--- 2025/day1/day1_part2.js
+++ 2025/day1/day1_part2.js
@@ -13,12 +13,16 @@ const file = readline.createInterface({
   safe with a dial, values 0 - 99; wraps around 99 -> 0
   given set of instructions with direction + distance of rotation - L23,R45,etc
   move to right = increase dial value, move left = decrease
-  find number of times where dial is at 0 after the given move
+  find number of times where dial passes or ends on 0 during a move
 */
 
 // dial starts at 50
 var dialAt = 50,
   count = 0;
+
+function modulo(x, n) {
+  return ((x % n) + n) % n;
+}
 
 // store all instructions
 file.on('line', (line) => {
@@ -26,10 +30,23 @@ file.on('line', (line) => {
 
   var distance = Number(line.substring(1));
 
-  // move dial
-  dialAt = (direction == 'R' ? dialAt + distance : dialAt - distance) % 100;
+  // count number of full rotations
+  count += Math.floor(distance / 100);
+  distance = distance % 100;
 
-  if (dialAt == 0) ++count;
+  var endAt = direction == 'R' ? dialAt + distance : dialAt - distance;
+
+  // console.log(line);
+  // console.log(`start from ${dialAt}; end at ${endAt}`);
+
+  // check if 0 was passed or hit by remaining distance
+  if (dialAt > 0 && (endAt >= 100 || endAt <= 0)) {
+    // console.log('passed 0');
+    count++;
+  }
+
+  dialAt = modulo(endAt, 100);
+  // console.log(`dial now at ${dialAt}`);
 });
 
 file.on('close', () => {
